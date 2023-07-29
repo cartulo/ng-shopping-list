ca5392ebc168d9953de8ca56b2bc4906b9554774
Gilberto Junior <contato.gil.tulo@gmail.com>
2023-07-29 12:36:08 -0300

    initial commit

---
+++ projects/tools/src/lib/tools.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'lib-tools',
+  template: `
+    <p>
+      tools works!
+    </p>
+  `
+})
+export class ToolsComponent {
+
+}
---
+++ projects/tools/src/lib/tools.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {ToolsComponent} from './tools.component';
+
+@NgModule({
+  declarations: [
+    ToolsComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    ToolsComponent
+  ]
+})
+export class ToolsModule { }
